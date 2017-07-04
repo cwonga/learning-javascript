46de82f4f3510bcfae6b9ff5fb45127ec11b3a70
Cathy Wong <cwonga@gmail.com>
2017-07-04 14:43:07 -0700

    Learning alert and document.write

---
+++ learning-javascript.js
@@ -0,0 +1,12 @@
+alert("Hello");
+document.write("<h1>Welcome!</h1>");
+
+var cathy = "hi, my name is cathy"
+
+document.write(cathy);
+
+alert(cathy);
+
+var puddles = "hi, my name is puddles"
+
+document.write("<br>" + puddles);
